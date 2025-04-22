748fc9297aa9b32c690668261b6850c282a67e85
ChaPepperDev <143850144+ChaPepperDev@users.noreply.github.com>
2025-04-22 20:44:23 -0300

    Update Venda.java

--- src/Venda.java
+++ src/Venda.java
@@ -1,1 +1,8 @@
+import java.util.ArrayList;
+import Java.util.List;
 
+public class Venda; {
+    private static List<String> historicoVendas = new ArrayList<>();
+
+    public static void processarVendas(Funcionario funcionario, Inventario inventario
+}
